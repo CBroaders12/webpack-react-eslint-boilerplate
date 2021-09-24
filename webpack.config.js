3981f55c91241e7dc96335316b5055de82119f3b
CBroaders <67294112+CBroaders12@users.noreply.github.com>
2021-09-24 16:19:53 -0400

    fix .jsx file extension in resolve

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
@@ -30,7 +30,7 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: ['.js', 'jsx'],
+    extensions: ['.js', '.jsx'],
   },
   devServer: {
     static: {
